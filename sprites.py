a5ead16e82007392dd72bfbbe5ad257d5fa174f4
waidhoferj <waidhoferj@gmail.com>
2022-06-04 15:09:24 -0700

    Updated sprite loading

--- sprites.py
+++ sprites.py
@@ -1,3 +1,4 @@
+from glob import glob
 import pyBaba
 import pygame
 import os
@@ -7,56 +8,18 @@ appdir = os.path.abspath(os.path.dirname(__file__))
 
 class SpriteLoader:
     def __init__(self):
-        self.icon_images = {
-            pyBaba.ObjectType.ICON_BABA: 'BABA',
-            pyBaba.ObjectType.ICON_FLAG: 'FLAG',
-            pyBaba.ObjectType.ICON_CRAB: 'CRAB',
-            pyBaba.ObjectType.ICON_ANNI: 'ANNI',
-            pyBaba.ObjectType.ICON_WATER: 'WATER',
-            pyBaba.ObjectType.ICON_FIRE: 'FIRE',
-            pyBaba.ObjectType.ICON_SKULL: 'SKULL',
-            pyBaba.ObjectType.ICON_DUST: 'DUST',
-            pyBaba.ObjectType.ICON_WALL: 'WALL',
-            pyBaba.ObjectType.ICON_ROCK: 'ROCK',
-            pyBaba.ObjectType.ICON_TILE: 'TILE',
-            pyBaba.ObjectType.ICON_TYPE: 'TYPE_replacement',
-            pyBaba.ObjectType.ICON_KEKE: 'KEKE',
-            pyBaba.ObjectType.ICON_LAVA: 'LAVA',
-            pyBaba.ObjectType.ICON_LOVE: 'LOVE',
-        }
 
-        for i in self.icon_images:
-            p, _ = os.path.split(__file__)
-            fp = f'{p}/sprites/icon/{self.icon_images[i]}.gif'
-            self.icon_images[i] = pygame.transform.scale(pygame.image.load(
-                fp), (BLOCK_SIZE, BLOCK_SIZE))
+        self.icon_images = {}
+        for path in glob("sprites/icon/*.gif"):
+            fname = os.path.split(path)[-1].split(".")[0]
+            obj_type = getattr(pyBaba.ObjectType, f"ICON_{fname}")
+            self.icon_images[obj_type] = pygame.transform.scale(pygame.image.load(
+                path), (BLOCK_SIZE, BLOCK_SIZE))
 
-        self.text_images = {
-            pyBaba.ObjectType.BABA: 'BABA',
-            pyBaba.ObjectType.IS: 'IS',
-            pyBaba.ObjectType.YOU: 'YOU',
-            pyBaba.ObjectType.FLAG: 'FLAG',
-            pyBaba.ObjectType.CRAB: 'CRAB',
-            pyBaba.ObjectType.FIRE: 'FIRE',
-            pyBaba.ObjectType.DUST: 'DUST',
-            pyBaba.ObjectType.WIN: 'WIN',
-            pyBaba.ObjectType.WALL: 'WALL',
-            pyBaba.ObjectType.FACING: 'FACING',
-            pyBaba.ObjectType.STOP: 'STOP',
-            pyBaba.ObjectType.ROCK: 'ROCK',
-            pyBaba.ObjectType.PUSH: 'PUSH',
-            pyBaba.ObjectType.SKULL: 'SKULL',
-            pyBaba.ObjectType.LAVA: 'LAVA',
-            pyBaba.ObjectType.MELT: 'MELT',
-            pyBaba.ObjectType.ANNI: 'ANNI',
-            pyBaba.ObjectType.KEKE: 'KEKE',
-            pyBaba.ObjectType.WATER: 'WATER',
-            pyBaba.ObjectType.MOVE: 'MOVE',
-            pyBaba.ObjectType.HOT: 'HOT',
-            pyBaba.ObjectType.LOVE: 'LOVE',
-        }
-
-        for i in self.text_images:
-            p, _ = os.path.split(__file__)
-            fp = f'{p}/sprites/text/{self.text_images[i]}.gif'
-            self.text_images[i] = pygame.transform.scale(pygame.image.load(fp), (BLOCK_SIZE, BLOCK_SIZE))
+        
+        self.text_images = {}
+        for path in glob("sprites/text/*.gif"):
+            fname = os.path.split(path)[-1].split(".")[0]
+            obj_type = getattr(pyBaba.ObjectType, fname)
+            self.text_images[obj_type] = pygame.transform.scale(pygame.image.load(
+                path), (BLOCK_SIZE, BLOCK_SIZE))
